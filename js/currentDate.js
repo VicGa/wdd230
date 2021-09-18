99f1f9cf56da42c87146bfbf369c187bed82e15a
VicGa <u262101642@byui.edu>
2021-09-17 22:46:14 -0300

    last current day

--- js/currentDate.js
+++ js/currentDate.js
@@ -1,2 +1,40 @@
-const options = {weekday:'long', day:'numeric', month:'long', year:'numeric'};
-document.getElementById('currentDate').textContent= new Date().toLocaleDateStringLocal('en-US', options);
+//day names array 
+const daynames = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday'
+];
+
+// month names array
+
+const monthnames = [
+    'january',
+    'february',
+    'march',
+    'april',
+    'may',
+    'june',
+    'july',
+    'august',
+    'september',
+    'october',
+    'november',
+    'december'
+];
+
+const todaysDate = new Date();
+const dayNames = daynames[todaysDate.getDay()];
+const monthNames = monthnames[todaysDate.getMonth()];
+const yearNames = todaysDate.getFullYear;
+const currentdate = dayNames + ', ' + todaysDate.getDate() + ' ' + monthNames + ', ' + todaysDate.getFullYear();
+
+
+
+
+
+
+document.getElementById('currentDate').textContent=currentdate;
