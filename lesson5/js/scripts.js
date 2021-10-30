64ea40bc1f5fc09c9fb2e2696f8f8ec8eb1e3817
VicGa <90812185+VicGa@users.noreply.github.com>
2021-10-30 20:50:20 -0300

    finished

--- lesson5/js/scripts.js
+++ lesson5/js/scripts.js
@@ -11,3 +11,11 @@ let mod_date = document.lastModified;
 
 modified.innerHTML = `Last Modified: ${mod_date}`;
 copyright.innerHTML = current_year;
+let day = d.getDay();
+if (day == 'Friday') {
+    document.getElementById("announce").innerHTML = "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion.";
+    document.getElementById("announce").style.display = "block";
+}
+else {
+    document.getElementById("announce").style.display = "none";
+}
