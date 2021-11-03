03fb7f11780667d612b8bad9663619148cd56998
VicGa <90812185+VicGa@users.noreply.github.com>
2021-11-03 20:36:01 -0300

    line 10 in js-change

--- lesson7/js/script.js
+++ lesson7/js/script.js
@@ -7,7 +7,7 @@ let modified = document.querySelector('#currentDate');
 let current_year = new Date().toLocaleDateString('en-US', options);
 let mod_date = document.lastModified;
 
-modified.innerHTML = `Last Modified: ${mod_date}`;
+modified.innerHTML = "Last Modified: ${mod_date}";
 copyright.innerHTML = current_year;
 
 
