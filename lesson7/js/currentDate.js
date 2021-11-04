fe7824ce97b062cbb978380b4a12142214e32912
VicGa <90812185+VicGa@users.noreply.github.com>
2021-11-03 21:22:47 -0300

    ert

--- lesson7/js/currentDate.js
+++ lesson7/js/currentDate.js
@@ -6,4 +6,4 @@ let current_year = new Date().toLocaleDateString('en-US', options);
 let mod_date = document.lastModified;
 
 modified.innerHTML = `${mod_date}`;
-copyright.innerHTML = current_year;
+// copyright.innerHTML = current_year;
