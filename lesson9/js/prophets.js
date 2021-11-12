6feb9b9e32fc5f0f47a858df088111a8e7d77d75
VicGa <90812185+VicGa@users.noreply.github.com>
2021-11-12 20:32:54 -0300

    finished prophets cards

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -14,10 +14,21 @@ fetch(requestURL)
     for (let i = 0; i < prophets.length; i++ ) {
         let card = document.createElement('section');
         let h2 = document.createElement('h2');
-
-        h2.textContent = prophets[i].name + ' ' + prophets[i].lastname + ' ' + prophets[i].birthDate + ' ' + prophets[i].birthPlace + ' ' + prophets[i]externalImage;
+        let image = document.createElement('img');
+        let birthDate = document.createElement('div');
+        let birthPlace = document.createElement('div');
+        h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+        birthDate.setAttribute('class', 'birth')
+        birthDate.textContent = 'Date of Birth: '+ prophets[i].birthdate;
+        birthPlace.setAttribute('class', 'birthPlace')
+        birthPlace.textContent = 'Place of birth: ' + prophets[i].birthplace;
+        image.setAttribute('src', prophets[i].imageurl);
+        image.setAttribute('alt', prophets[i].name + ' ' + prophets[i].lastname);
 
         card.appendChild(h2);
+        card.appendChild(birthDate);
+        card.appendChild(birthPlace);
+        card.appendChild(image);
 
         document.querySelector('div.cards').appendChild(card);
 
